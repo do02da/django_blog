4cc20e177839cd1a9b99dff2ead1acfbe5315c5d
Kim doyeong <raze0209@gmail.com>
2021-11-03 10:37:48 +0900

    add auth

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +9,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name="post_detail"),
     path('post/new/', views.post_new, name="post_new"),
     path('post/<int:pk>/edit/', views.post_edit, name="post_edit"),
+    path('post/<int:pk>/remove', views.post_remove, name="post_remove"),
+    path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name="logout"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -14,6 +15,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -28,6 +30,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -41,3 +44,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
